ccbe49a86cd8766c4ac7c0c1490ff684c6893647
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2018-08-20 16:19:46 +0900

    fix indent style

--- source/app.d
+++ source/app.d
@@ -2,9 +2,9 @@ import std.stdio;
 import tvm.parser;
 
 void main() {
-	string[] test_cases = [
-		"var foo;", `var foo = "value";`, "var foo = variable;",
-		`function funcName(a, b) {
+  string[] test_cases = [
+    "var foo;", `var foo = "value";`, "var foo = variable;",
+    `function funcName(a, b) {
       a = b;
       a + b;
       a - b;
@@ -18,21 +18,21 @@ void main() {
       function add(a, b) { return a + b; }
       add(a, b);
     }, "true", q{
-			if (1 == a) {
-				abc();
-			}
-		}, q{
-			if (a == b) {
-				return abc();
-			} else {
-				return def();
-			}
-		}, "a || b"
-	];
+      if (1 == a) {
+        abc();
+      }
+    }, q{
+      if (a == b) {
+        return abc();
+      } else {
+        return def();
+      }
+    }, "a || b"
+  ];
 
-	foreach (test_case; test_cases) {
-		writeln("----------------------------");
-		writeln("source: ", test_case);
-		writeln(JS(test_case).buildAST);
-	}
+  foreach (test_case; test_cases) {
+    writeln("----------------------------");
+    writeln("source: ", test_case);
+    writeln(JS(test_case).buildAST);
+  }
 }
