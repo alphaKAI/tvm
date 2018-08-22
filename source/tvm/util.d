d962a80cb82bfc39953daf9716f95973691aef42
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2018-08-22 18:30:46 +0900

    util/Stack

---
+++ source/tvm/util.d
@@ -0,0 +1,37 @@
+module tvm.util;
+import std.array;
+
+struct Stack(T) {
+  T[] stack;
+
+  @property T pop() {
+    T t = stack[$ - 1];
+    stack.length--;
+    return t;
+  }
+
+  @property size_t length() {
+    return stack.length;
+  }
+
+  @property void push(T value) {
+    stack ~= value;
+  }
+
+  @property bool empty() {
+    return stack.empty;
+  }
+
+  @property T front() {
+    return pop;
+  }
+
+  @property void popFront() {
+    pop;
+  }
+
+  @property void popAll() {
+    foreach (_; this) {
+    }
+  }
+}
